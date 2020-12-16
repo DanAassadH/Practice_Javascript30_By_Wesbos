c1b1a0fb1357515845c92f298d46f26a6c1f85ac
Danish <danishriz100@gmail.comgit commitexit>
2020-12-16 10:56:46 -0700

    slide in on scroll

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -55,7 +55,7 @@ progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
 
-fullScreen.addEventListener('click', () => {
-    console.warn("fullscreen");
+fullScreen.addEventListener('click', () => { 
+   // console.warn("fullscreen");
     video.requestFullscreen();
 } );
